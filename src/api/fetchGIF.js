134e31e4229ccbafabd36f49b9f3e48fee8afbe5
Kaosc <themelihbt@gmail.com>
2022-12-19 16:41:58 +0000

    update

--- src/api/fetchGIF.js
+++ src/api/fetchGIF.js
@@ -1,5 +1,7 @@
 import axios from "axios"
 import dotenv from "dotenv"
+import arrayShuffle from 'array-shuffle';
+import randomNumber from 'random'
 
 // Load secrets (Only for local env)
 dotenv.config()
@@ -9,63 +11,54 @@ const TENOR_CLIENT_KEY = process.env.TENOR_CLIENT_KEY
 const GIPHY_API_KEY = process.env.GIPHY_API_KEY
 
 // TENOR
-const fetchTenor = async (SEARCH, RANDOM) => {
-	let gifUrls = []
-	const LIMIT = SEARCH !== null ? "20" : "50" // default: 20 & max: 50
+const fetchTenor = async (search, limit, random) => {
+  let gifUrls = []
+  const URL =
+    "https://tenor.googleapis.com/v2/search?q=" + search +
+    "&key=" + TENOR_API_KEY +
+    "&client_key=" + TENOR_CLIENT_KEY +
+    "&limit=" + limit +
+    "&random=" + random
 
-	const URL =
-		"https://tenor.googleapis.com/v2/search?q=" +
-		SEARCH +
-		"&key=" +
-		TENOR_API_KEY +
-		"&client_key=" +
-		TENOR_CLIENT_KEY +
-		"&limit=" +
-		LIMIT +
-		"&random=" +
-		RANDOM
+  await axios
+    .get(URL)
+    .then((response) => {
+      gifUrls = response.data.results.map((gif) => gif.url)
+    })
+    .catch((e) => console.log("err while fetchin from tenor: ", e))
 
-	await axios
-		.get(URL)
-		.then((response) => {
-			gifUrls = response.data.results.map((gif) => gif.url)
-		})
-		.catch((e) => console.log("err while fetchin from tenor: ", e))
-
-	return gifUrls
+  return gifUrls
 }
 
 // GIPHY
-const fetchGiphy = async (SEARCH) => {
-	let gifUrls = []
-	const LIMIT = "25" // default: 25 & max: ?
+const fetchGiphy = async (search, limit) => {
+  let gifUrls = []
+  let URL =
+    "https://api.giphy.com/v1/gifs/search?" +
+    "api_key=" + GIPHY_API_KEY +
+    "&q=" + search +
+    "&limit=" + limit +
+    "&offset=0&rating=g&lang=en"
 
-	const URL =
-		"https://api.giphy.com/v1/gifs/search?api_key=" +
-		GIPHY_API_KEY +
-		"&q=" +
-		SEARCH +
-		"&limit=" +
-		LIMIT +
-		"&offset=0&rating=g&lang=en"
-
-	await axios
-		.get(URL, {headers: { "Accept-Encoding": "gzip,deflate,compress" } })
-		.then((response) => {
+  await axios
+    .get(URL, { headers: { "Accept-Encoding": "gzip,deflate,compress" } })
+    .then((response) => {
       gifUrls = response.data.data.map((gif) => gif.images.original.url)
     })
-		.catch((e) => console.log("err while fetchin from giphy: ", e))
+    .catch((e) => console.log("err while fetchin from giphy: ", e))
 
-	return gifUrls
+  return gifUrls
 }
 
 export const getGIF = async (q) => {
-	const SEARCH = q ? "cat" + " " + q.value : "cat"
-	const RANDOM = q ? true : false
 
-	const giphyUrls = await fetchGiphy(SEARCH)
-	const tenorUrls = await fetchTenor(SEARCH, RANDOM)
-	const urlList = tenorUrls.concat(giphyUrls)
+  const search = q ? "cat" + " " + q.value : "cat"
+  const limit = q ? "20" : "50"
+  const random = q ? false : true
 
-	return urlList[Math.floor(Math.random() * urlList.length)]
+  const giphyUrls = await fetchGiphy(search, limit)
+  const tenorUrls = await fetchTenor(search, limit, random)
+  const urlList = arrayShuffle(tenorUrls.concat(giphyUrls))
+
+  return urlList[randomNumber.int(0, urlList.length)]
 }
