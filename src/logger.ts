27df3702957fb03adba3438ff98019aed5a52d1a
Kaosc <themelihbt@gmail.com>
2023-09-05 00:39:56 +0300

    update

--- src/logger.ts
+++ src/logger.ts
@@ -1,7 +1,13 @@
 import fs from "fs"
 
 export const saveLog = (error: string, type: string) => {
+	if (!fs.existsSync(`./src/logs/log-${type}.txt`)) {
+		fs.writeFile(`./src/logs/log-${type}.txt`, "", function (err) {
+			if (err) console.error(err)
+		})
+	}
+
 	fs.appendFile(`./src/logs/log-${type}.txt`, `${error}\n`, function (err) {
-		if (err) throw err
+		if (err) console.error(err)
 	})
 }
